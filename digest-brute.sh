f83e5f52529fc5fcbb98029fc5c339a3e5dc9601
Grant Sowards <gsowards100@gmail.com>
2018-10-18 19:54:17 -0600

    updates

--- digest-brute.sh
+++ digest-brute.sh
@@ -6,6 +6,8 @@ GRN='\033[0;32m'
 NC='\033[0m' 
 MG='\033[0;34m'
 YW='\033[1;33m'
+LB='\033[1;36m'
+WB='\033[1;37m'
 
 password=4425
 username=infomte
@@ -19,14 +21,14 @@ qop=auth
 
 
 clear
-echo -e "${YW}Calculating Digest Authentication Response *** ${NC}\r\n"
+echo -e "${YW}Calculating Digest Authentication Response${NC}\r\n"
 echo -e "Calculating H1 using ${MG}MD5($username:$realm:$password) ${NC}"
   hash1="$(echo -n $username:$realm:$password | md5sum | awk '{print $1}')"
-  echo -e "${RED}--->hash1: ${GRN}$hash1${NC}\r\n"
+  echo -e "${RED}--->hash1: ${LB}$hash1${NC}\r\n"
 echo -e "Calculating H2 using ${MG}MD5($method:$url)${NC}"
   hash2="$(echo -n $method:$url | md5sum | awk '{print $1}')"
-  echo -e "${RED}--->hash2: ${GRN}$hash2${NC}\r\n"
-echo -e "Calculating response using: \r\n${MG}MD5($hash1${YW}:${MG}$nonce${YW}:${MG}\r\n$nc${YW}:${MG}$cnonce${YW}:${MG}$qop${YW}:${MG}$hash2)${NC}"
+  echo -e "${RED}--->hash2: ${WB}$hash2${NC}\r\n"
+echo -e "Calculating response using: \r\n${MG}MD5(${LB}$hash1${YW}:${MG}$nonce${YW}:${MG}\r\n$nc${YW}:${MG}$cnonce${YW}:${MG}$qop${YW}:${WB}$hash2)${NC}"
   calculated="$(echo -n $hash1:$nonce:$nc:$cnonce:$qop:$hash2 | md5sum | cut -d " " -f1)"
 echo -e "${RED}--->Response:${GRN}$calculated${NC}\r\n"
 
