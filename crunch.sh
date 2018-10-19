8b45a8c37e32fa02321d5d025ef79952a299207d
root <root@localhost.localdomain>
2018-10-18 19:18:32 -0600

    updates

---
+++ crunch.sh
@@ -0,0 +1,2 @@
+crunch 4 4 ttes -t @@@@ -o words.txt
+
--- digest-brute.sh
+++ digest-brute.sh
@@ -7,28 +7,25 @@ NC='\033[0m'
 
 password=4425
 username=infomte
-realm=members only
+realm=members\ only
 method=GET
-url="/"
+url=/
 nonce=cPmikbouBQA=92809400a6834c1fbc7b098b91e0fe759da84dcd
-nc=00000002
+nc=00000001
 cnonce=760a2297807a0a51
 qop=auth
 
 
 clear
-echo "************************************************"
-echo " 		      Digest Bruteforce              *"
-echo "************************************************"
-echo "H1 = MD5($username:$realm:$password)"
+echo "H1:  MD5($username:$realm:$password)"
   hash1="$(echo -n $username:$realm:$password | md5sum | awk '{print $1}')"
-#  echo hash1: $hash1
-echo "H2 = MD5($method:url)"
+  echo -e "hash1: ${GRN}$hash1${NC}"
+echo "H2:  MD5($method:$url)"
   hash2="$(echo -n $method:$url | md5sum | awk '{print $1}')"
-#  echo hash2: $hash2
+  echo -e "hash2: ${GRN}$hash2${NC}"
 echo "response = MD5($hash1:$nonce:$nc:$cnonce:$qop:$hash2)"
   calculated="$(echo -n $hash1:$nonce:$nc:$cnonce:$qop:$hash2 | md5sum | cut -d " " -f1)"
-echo "${GRN}$response${NC}"
+echo -e "${GRN}$calculated${NC}"
 
 
 
